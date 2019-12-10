7a47569ae036c8a4a54a0f186ab64dc242f69015
josezamora <josezamora@192.168.1.9>
2019-12-10 18:28:29 -0500

    commit fin clases

--- src/patron1/Intermediary.java
+++ src/patron1/Intermediary.java
@@ -5,13 +5,58 @@
  */
 package patron1;
 import jade.core.Agent;
+import jade.BootProfileImpl;
+import jade.core.Runtime;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.util.leap.Properties;
+import jade.wrapper.*;
+
 /**
  *
  * @author josezamora
  */
-public class Intermediary extends Agent{
 
-   singleAgent agent1 = new singleAgent();
-   singleHuman human1 = new singleHuman(); 
    //este es un cambio
+
+public class Intermediary {
+
+     public static void main(String args[]) throws StaleProxyException 
+     {
+        Runtime rt = Runtime.instance();
+
+	// Exit the JVM when there are no more containers around
+
+	rt.setCloseVM(true);
+
+	// Create a default profile
+
+	Profile profile = new ProfileImpl("localhost", 1099, "main");
+
+	AgentContainer mainContainer = jade.core.Runtime.instance().createMainContainer(profile);
+
+        Object[] hum=new Object[20];
+        hum[0]="humano";
+        
+        Object[] ag=new Object[20];
+        ag[0]="agente";
+       
+
+	AgentController rma = null;
+        AgentController mma = null;
+
+	try {
+
+		rma = mainContainer.createNewAgent("agente", "patron1.singleAgent", hum);
+		rma.start();
+                mma = mainContainer.createNewAgent("humano", "patron1.singleHuman",new Object[0]);
+                mma.start();
+
+	} catch (StaleProxyException e) {
+            
+		e.printStackTrace();
+	}
+
+     }
+
 }
